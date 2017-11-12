c1826feaf5d112e3744b7e2ff86180712edb6ac5
Elliot Lin <elliotlin@rocketmail.com>
2017-11-11 16:01:02 -0800

    Test deploy to heroku

--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
+from flask import abort
+from flask import json
+from flask import jsonify
+from flask import request
 from app import app
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return jsonify(**{ 'reply': 'Hello, World!'})
+
+@app.route('/v1/application/processReceipt')
+def process_receipt():
+    data = request.get_json()
+    receipt_raw = data['content']
+    print(receipt_raw)
