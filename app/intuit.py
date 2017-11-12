c72971682dbfae10869e1ed351528e188e18f0c6
Elliot Lin <elliotlin@rocketmail.com>
2017-11-12 03:00:01 -0800

    OAuth2 gives me keys now

--- app/intuit.py
+++ app/intuit.py
@@ -7,7 +7,7 @@ PURCHASE_OP='/v3/company/'+COMPANY_ID+'/purchase'
 CLIENT_ID='Q09pGXudM4vOLRL2wfTe5NLbEMkZWLlWYicaGAJoBchIkgAEni'
 CLIENT_SECRET='DgCyVEAXFeaQh761YGNi0Iv9HOnRKm17LtPABn2R'
 
-REDIRECT_URI_DEV='/oauth_authorized'
+REDIRECT_URI_DEV='oauth_authorized'
 REDIRECT_URI_PROD='http://inventory-ba-backend.herokuapp.com/oauth_authorized'
 AUTH_BASE_URL='https://appcenter.intuit.com/connect/oauth2'
 ACCESS_BASE_URL='https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer'
--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from flask import abort
 from flask import json
 from flask import jsonify
+from flask import redirect
 from flask import request
 from flask import session
 from flask import url_for
 from app import app
 from app import qbo
-from .intuit import BASE_URL, PURCHASE_OP, REDIRECT_URI_PROD
+from .intuit import BASE_URL, PURCHASE_OP, REDIRECT_URI_DEV
 from .costco_regex import receipt_reader
 
 import json
@@ -25,8 +26,10 @@ def process_receipt():
     # do some processing here
     #body = create_request_body(receipt_raw)
     # Now create a new purchase object to Quickbooks
+    if 'access_token' not in session:
+        redirect('/auth')
     headers = {'Content-Type': 'application/json'}
-    headers['Authorization'] = 'Bearer eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0.._v9eWtvSGfTAE12wRyLEfw.5X5c9fK4N-gRnR_45t08gVDdBnoyqOR5gb-O6_IuQ5PRgwOAVE7TMbIu2EaZZfzWAtqzdeWG0Y8wPpJYoLCXwD8xY63pUvuf0LKMwRW3K-9jCsEsbiMY4UKSk_QdbBG8zbIsAMSpE7d9m8wdVu8TJVxcAl2ouZt29xGnAQnn-VSWQWEFAkv4P0UGvo9tVGcDiROxMCudA3kpFRCnqBl0ECmNs2jCwLl5FVjKrNKPJL6hUJEGIXPpIaYH7zeuJZQ9ixH_IwkkosYbufo1yalZ5G_G22XyFIifl5jpXIrLfSOFRJU1ddyX20Q4pwcBiS2vqZGbKB89-S0E84ph2G19f4kzI7Q_VkSVz7ON_JZ5gqXF-9qywwrxv3YkWMj0RE1bIlw_2UJpTCxdkQZ1z1mDKnTVtXmrvg9DD-wpCSjlzDpsQ1zleyn6joSvzSYq4Sxx5Jr_MDigxnVZLDKKjXffOWnSRfxshjVyMgLro-43JXBiihplpfBfRtMUwyAuKoirL32hIpcv-2DfxqTx1S1PyOWlihvPF4PO72AoogAugdR02fVTkForMMnUbPyFpp0hueRgNpaHXk5eHHCc1Dgf24fsOK3XerAB1A2XEAoDmbCjK6tcwgIamTLUkxkbcn9vASExRCdsLaX6USfVgk8SI78cc70YFqCv14AlgTEklPJek99Qirj_lmJKny2ilIsZ.wRqo8S9C-nYLpszygDsgbw'
+    headers['Authorization'] = 'Bearer ' + session['access_token']
     #r = qbo.post(PURCHASE_OP, data={}, headers=headers)
     payload = json.dumps(receipt_reader())
     r = requests.post(BASE_URL + PURCHASE_OP, headers=headers, data=payload)
@@ -35,7 +38,7 @@ def process_receipt():
 
 @app.route('/auth')
 def auth():
-    return qbo.authorize(callback=url_for(REDIRECT_URI_PROD))
+    return qbo.authorize(callback=url_for(REDIRECT_URI_DEV))
 
 @app.route('/oauth_authorized')
 @qbo.authorized_handler
@@ -45,13 +48,10 @@ def oauth_authorized(resp):
             'authStatus': 'Fail',
             'errorResp': request.args.get('error'),
         })
-    realm_id = str(request.args.get('realmId'))
-    state = str(request.args.get('state'))
-    code = str(request.args.get('code'))
-    print(code)
-    if resp is None or state is not 'MERDE':
-        print('You denied the request to sign in.')
+    if resp is None:
         return jsonify(**{'authStatus': 'Fail'})
     # Setting the session using flask session just for prototyping
+    keys = resp.get_json()
     session['is_authorized'] = True
-    session['realm_id'] = realm_id
+    session['access_token'] = keys.get('access_token')
+    return jsonify(**{'authStatus': 'Success'})
