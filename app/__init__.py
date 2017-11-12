15551e89c04f1d3238ee023dd0463fb35f147a27
Elliot Lin <elliotlin@rocketmail.com>
2017-11-11 16:18:50 -0800

    CORS??

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 # Create flask app and configure it
 app = Flask(__name__)
 app.config.from_object('app.config.DevConfig')
+
+# Allow CORS
+CORS(app)
 
 # Init SQLAlchemy engine
 db = SQLAlchemy(app)
