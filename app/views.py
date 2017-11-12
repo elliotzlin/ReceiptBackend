53aeb16c8bc41e2ed307f8f9270b7ea85c8d6e22
Elliot Lin <elliotlin@rocketmail.com>
2017-11-11 16:27:04 -0800

    Some error handling

--- app/views.py
+++ app/views.py
@@ -8,8 +8,10 @@ from app import app
 def index():
     return jsonify(**{ 'reply': 'Hello, World!'})
 
-@app.route('/v1/application/processReceipt', methods=['POST'])
+@app.route('/v1/application/processReceipt', methods=['GET', 'POST'])
 def process_receipt():
     data = request.get_json()
+    if not data:
+        return jsonify(**{'postData': 'No data'})
     receipt_raw = data['content']
-    print(receipt_raw)
+    return jsonify(**{ 'postData': receipt_raw })
